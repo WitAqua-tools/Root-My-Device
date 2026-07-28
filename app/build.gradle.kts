15d25a6c4f429c94d2f4390095ae27b40be1e710
soralis0912 <m.mt.br.527@gmail.com>
2026-07-28 18:34:38 +0900

    Number the first signed release 0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 8
-        versionName = "0.2.3"
+        versionName = "0.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
